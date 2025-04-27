3e05322097be503e39ed199528a93369c20d7d40
RAHOU Meriem <lm_rahou@esi.dz>
2025-04-27 23:03:55 +0100

    Initial Commit

---
+++ QuickToDo/QuickToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickToDoApp.swift
+//  QuickToDo
+//
+//  Created by mery Rahou on 27/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuickToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
